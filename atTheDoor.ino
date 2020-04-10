c993ac3586424fbf58d192b04b17f2fa6f628e4f
Hansa Jayathilaka <57087579+hansajayathilaka@users.noreply.github.com>
2020-04-10 13:54:53 +0530

    Delete atTheDoor.ino

--- atTheDoor.ino
+++
@@ -1,8 +0,0 @@
-void atTheDoor() {
-  char temp = recieveData();
-  if (temp != '_') {
-    destination = temp;
-    path_to_next_destination = createPath(currentPosition, destination) + "B";
-    junctionIndex = 0;
-  }
-}
